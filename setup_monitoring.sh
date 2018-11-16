765f61cc4f804fbc60d69ff2a0e303d9133c3c13
Botond Dénes <bdenes@scylladb.com>
2018-11-16 11:08:31 +0200

    setup_monitoring.sh: specify zone explicitely

--- setup_monitoring.sh
+++ setup_monitoring.sh
@@ -2,6 +2,6 @@
 
 source ./config.sh
 
-gcloud compute ssh ${MONITORING_NODE} --command="sudo docker stop aalert agraf aprom"
-gcloud compute ssh ${MONITORING_NODE} --command="sudo docker rm aalert agraf aprom"
-gcloud compute ssh ${MONITORING_NODE} --command="cd scylla-grafana-monitoring && sudo rm -rf myconfig && sudo ./genconfig.py -ns -d ./myconfig/ ${NODES} && sudo ./start-all.sh -s ./myconfig/scylla_servers.yml -n ./myconfig/node_exporter_servers.yml"
+gcloud compute ssh ${MONITORING_NODE} --zone=${ZONE} --command="sudo docker stop aalert agraf aprom"
+gcloud compute ssh ${MONITORING_NODE} --zone=${ZONE} --command="sudo docker rm aalert agraf aprom"
+gcloud compute ssh ${MONITORING_NODE} --zone=${ZONE} --command="cd scylla-grafana-monitoring && sudo rm -rf myconfig && sudo ./genconfig.py -ns -d ./myconfig/ ${NODES} && sudo ./start-all.sh -s ./myconfig/scylla_servers.yml -n ./myconfig/node_exporter_servers.yml"
