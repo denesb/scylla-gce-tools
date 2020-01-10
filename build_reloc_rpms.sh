7a231773da5013be474cec0686e75ea4065a9c46
Botond Dénes <bdenes@scylladb.com>
2020-01-10 10:00:26 +0200

    build_reloc_rpms.sh: run install-dependencies.sh for each component

--- build_reloc_rpms.sh
+++ build_reloc_rpms.sh
@@ -3,6 +3,7 @@
 source ./config.sh
 
 pushd ${SCYLLA_REPO}
+./install-dependencies.sh
 ./reloc/build_reloc.sh
 ./reloc/build_rpm.sh
 ./reloc/python3/build_reloc.sh
@@ -10,11 +11,13 @@ pushd ${SCYLLA_REPO}
 popd
 
 pushd ${SCYLLA_TOOLS_JAVA_REPO}
+./install-dependencies.sh
 ./reloc/build_reloc.sh
 ./reloc/build_rpm.sh
 popd
 
 pushd ${SCYLLA_JMX_REPO}
+./install-dependencies.sh
 ./reloc/build_reloc.sh
 ./reloc/build_rpm.sh
 popd
